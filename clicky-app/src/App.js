d83829da77a2f8b7fd09f8826a0ecfd0e8bc7e84
Joe Christopher <joechristopher@icloud.com>
2018-05-13 02:07:05 -0400

    functionality working, needs more work

--- clicky-app/src/App.js
+++ clicky-app/src/App.js
@@ -1,39 +1,15 @@
-import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
+import React from "react";
+import Jumbotron from "./components/Jumbotron";
+import Game from "./components/Game";
 import "./App.css";
 
-class App extends Component {
-  // Setting this.state.friends to the friends json array
-  state = {
-    friends
-  };
+const App = () => (
+  <div className= "siteLayout">
+    <Jumbotron />
+    <Game /> 
+  </div>
+);
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
 
-  // Map over this.state.friends and render a FriendCard component for each friend object
-  render() {
-    return (
-      <Wrapper>
-        <Title>Friends List</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            image={friend.image}
-          />
-        ))}
-      </Wrapper>
-    );
-  }
-}
 
 export default App;
---
+++ clicky-app/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = () => (
+    <div className="jumbotron">
+        <h1 className="displaylogo">Philadelphia Eagles Memory Game</h1>
+        <p className="gamedirections">Click on a player to score a point, click on the same player more than once and you lose!</p>
+    </div>
+);
+
+export default Jumbotron;
