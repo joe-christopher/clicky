a7068752a762aefed26ce79899c0cd83ac79aa62
Joe Christopher <joechristopher@icloud.com>
2018-05-14 10:22:20 -0400

    added deployment details

--- clicky-app/src/App.js
+++ clicky-app/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Jumbotron from "./components/Jumbotron";
 import Game from "./components/Game";
-import "./App.css";
+
 
 const App = () => (
   <div className= "siteLayout">
